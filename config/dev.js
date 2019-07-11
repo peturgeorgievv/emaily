081fc9da660d9552af8dba9c05bdcaf92764198d
Петър Георгиев <peturgeorgievv@gmail.com>
2019-07-11 22:51:21 +0300

    finished google auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '909265856746-r1l6p1oc4q4kkbltnbqh1tt4a1d35gvu.apps.googleusercontent.com',
+  googleClientSecret: 'YiWyEXGREog_jLBlf893jnaY',
+  mongoURI:
+    'mongodb+srv://petur:1T1siM4eUAlZ@emaily-ducyr.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'jarenbeakjngrea'
+};
