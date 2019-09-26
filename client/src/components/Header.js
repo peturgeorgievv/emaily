77748f109fe6c04b63a88b2107dfd9356a468510
Петър Георгиев <peturgeorgievv@gmail.com>
2019-09-26 20:58:14 +0300

    Added additional path for express and heroku

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,7 +17,10 @@ class Header extends Component {
       default:
         return (
           <React.Fragment>
-            <li key="1"><Payments /></li>,
+            <li key="1"><Payments /></li>
+            <li key="3" style={{ margin: '0 10px' }}>
+              Credits: {this.props.auth.credits}
+            </li>
             <li key="2"><a href="/api/logout">Logout</a></li>
           </React.Fragment>
         );
