b1416dc6b8a619659c851de755974dc68b9e6fbd
Петър Георгиев <peturgeorgievv@gmail.com>
2019-09-27 17:39:52 +0300

    Added recipient schema

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecpientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recipients: [String],
+    recipients: [RecpientSchema],
     yes: { type: Number, default: 0},
     no: { type: Number, default: 0},
 });
