1133d31f98e617caccbd8017ea27a4d8181641a4
Петър Георгиев <peturgeorgievv@gmail.com>
2019-10-01 22:38:54 +0300

    Added SendGrid API and boilerplate for Mailer class

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,13 +6,15 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-         const { title, subject, body, recipients } = req.body;
+        const { title, subject, body, recipients } = req.body;
 
-         const survey = new Survey({
-             title,
-             subject,
-             body,
-             
-         })
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now(),
+        })
     });
 };
