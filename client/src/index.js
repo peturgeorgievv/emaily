8891c454724c157f9d474cdecd119fa7b0319a31
Петър Георгиев <peturgeorgievv@gmail.com>
2019-10-13 00:02:55 +0300

    Refactoring form

--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,6 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-// Remove later
-import axios from 'axios';
-window.axios = axios;
-
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
