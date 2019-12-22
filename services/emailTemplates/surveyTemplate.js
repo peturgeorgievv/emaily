b461528ad2db8cf8771602ddcc57969d62cfe4e9
Петър Георгиев <peturgeorgievv@gmail.com>
2019-12-22 15:59:03 +0200

    Changed email input and sending emails in productions

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -5,9 +5,12 @@ module.exports = (survey) => {
      <html>
         <body>
             <div style="text-align: center;">
-                <h3>I'd like your input!</h3>
+                <h3>Your vote is important to us!</h3>
+                <br />
                 <p>Please answer the following question:</p>
+                <br />
                 <p>${survey.body}</p>
+                <br />
                 <div>
                     <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
                 </div>
